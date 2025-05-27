02368a83b8ed3cf9c05a116f1d8ca85058f77d70
旭东 陈 <yincangshiwei@163.com>
2025-05-27 16:50:42 +0800

    调整结构

---
+++ nodes/__init__.py
@@ -0,0 +1,9 @@
+
+from .image.image_crop_alpha_node import ImageCropByAlphaAdvanced
+NODE_CLASS_MAPPINGS = {
+    "ImageCropByAlphaAdvanced": ImageCropByAlphaAdvanced,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ImageCropByAlphaAdvanced": "ImageCropA (Image)",
+}
