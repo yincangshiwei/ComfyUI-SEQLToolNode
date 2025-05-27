442ad3548a35175d95988113cb847ad86cdb723c
旭东 陈 <yincangshiwei@163.com>
2025-05-27 17:20:05 +0800

    修改节点名

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -5,5 +5,5 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ImageCropByAlphaAdvanced": "ImageCropA (Image)",
+    "ImageCropByAlphaAdvanced": "ImageCropByAlphaAdvanced (Image)",
 }
